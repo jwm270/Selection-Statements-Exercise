399fb0f79e402e366428bdeb380f103b965ae58e
Jimmy Moore <jwm270@gmail.com>
2025-05-06 13:55:40 -0500

    exercise Selection 1

---
+++ Selection-Statements-Exercise/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Selection_Statements_Exercise
+{
+
+//Create a program that allows a user to play a game where they must guess what your favorite number is:
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+
+            var favNumber = r.Next(1, 100);
+
+            Console.WriteLine("Guess my favorite number:");
+
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (favNumber < userInput)
+            {
+                Console.WriteLine("Too High");
+            }
+            else if (userInput < favNumber)
+            {
+                Console.WriteLine("Too Low");
+            }
+            else
+            {
+                Console.WriteLine("You're psychic!");
+            }
+        }
+
+    }
+}
